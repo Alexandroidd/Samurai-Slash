f7b85df1c23dfa93257919f189d9a0c2acece3da
alexandroidd <alexandroidcreighton@gmail.com>
2017-03-29 14:06:40 -0600

    adds successful False Start aspect to game and the bangBox will load if no one clicks early.  --- now need to start mapping player functions and comparing reaction times.

--- main.js
+++ main.js
@@ -1,3 +1,87 @@
+//////////////////////////////////
+//******************************//
+//*******GAME FUNCTION DATA*****//						
+//******************************//
+//////////////////////////////////
+
+
+//----When to start Listening for False Start------//
+//---Had to put this outside the ready button because it was firing--//
+//---false start when it should just be ready-----//
+
+
+
+
+
+
+///----This is the FALSE START MECHANISM -------////
+//-----------------------------------------------///
+//--this function stops the timeout, removes the event
+//----listener and restarts the game----------//
+var falseStart = function(x) {
+	stopBang(x);
+	console.log("FALSE START has activated");
+	restart();
+};
+//--------------------//
+
+
+//This is TIME TO GO --- This function initiates the TIMEOUT of Bang.
+//This had to be separated out from the readyButton because it was
+//firing simultaneously as the game was being started.
+
+var timeToGo = function() {
+	console.log("initiating...");
+	return (initiateBang());
+};
+//------------//
+
+
+//-------------Initiate-The-bangBox----------//
+//--this-begins-the-whole-shebang-mode---only-occurs-if-no-false-start//
+var initiateBang = function(){
+		var pause = window.setTimeout(bang, waitingPeriod);
+		return pause;
+	};
+//-------------------------------------------//
+				
+				
+
+//-----Stops the BOX from appearing ------//
+var stopBang = function(x){
+		clearTimeout(x);
+	};
+//-----------------------------------------//
+
+			
+
+//----This function gets the starting time of the Duel----//
+var getTime = function(event){
+	//this is where to save the timestamp into a variable
+	var startTime = Math.floor((event.timeStamp));
+	console.log(startTime);
+	return(startTime);
+};
+//--------------------------------//
+
+
+
+//-----RESTART FUNCTION-----//
+var restart = function() {
+	waitingPeriod = Math.floor(Math.random() * (9000 - 3000)) + 3000;
+	startTime = 0;
+	readyButton.style.opacity = '1';
+	console.log("a restart has been initiated");
+	document.removeEventListener('click',falseStart);
+
+};
+
+
+
+
+
+
+
 
 
 // -----PLAYER DATA ----//
@@ -14,9 +98,9 @@ var playerTwo = {
 
 ///////OBJECT DATA//////
 var readyButton = document.getElementById('ready');
-var isWorking = false;
-var badStart = false;
-
+var playerZone = document.getElementById('player2');
+var timeToAlert;
+// var startListening = false;
 
 
 
@@ -37,18 +121,35 @@ var waitingPeriod = Math.floor(Math.random() * (9000 - 3000)) + 3000;
 readyButton.addEventListener('click', function(){
 	console.log(waitingPeriod);
 	console.log("I'm Ready!");
-	initiateBang();
-	document.addEventListener('click', falseStart); // <--- can add function with two separate event listeners for each player's key.
+      console.log("I'M LISTENING...");
+      timeToAlert = true;
+      var kickOut = timeToGo();
+      playerZone.addEventListener('click', function() {
+      		falseStart(kickOut);
+
+      }); // <--- can add function with two separate event listeners for each player's key.
+
 	readyButton.style.opacity = '0';
 });
 
-var falseStart = function() {
-	stopBang();
- 	document.removeEventListener('click',falseStart);
-	console.log("FALSE START TRY AGAIN");
 
 
-};
+
+if(timeToAlert === true) {
+	timeToGo();
+}
+
+
+
+
+
+
+// var listening = function(){
+//       document.addEventListener('click', falseStart); // <--- can add function with two separate event listeners for each player's key.
+//       console.log("I'M LISTENING...");
+// };
+// listening();
+
 
 //---*tThis function makes the GO/ALERT box to shock player --//
 //----------REQUIRES the ready button to be clicked which ---//
@@ -58,10 +159,6 @@ bangBox();
   console.log("BANG is Working!");
 };
 //------------------------------------------------------------//
-//***********************************************************//
-
-
-
 
 //-----so, bang() just passes off the baton to bangBox, which actually----//
 //-----creates the box which exclaims that is is time to react------------//
@@ -78,65 +175,9 @@ bangBox = function() {
 
 
 
-//----This function gets the starting time of the Duel----//
-var getTime = function(event){
-	//this is where to save the timestamp into a variable
-	var startTime = Math.floor((event.timeStamp));
-	console.log(startTime);
-	return(startTime);
-};
-
-var earlyRestart = function(){
-	
-
-	waitingPeriod = Math.floor(Math.random() * (9000 - 3000)) + 3000;
-	startTime = 0;
-	readyButton.style.opacity = '1';
-	console.log("FALSE START TRY AGAIN");
-};
 
 
 
-//so this function is dependent on whether the startBox has appeared or not
-//if the box has NOT appeared, this function adds an event listener for player
-//input.-------------
-//this could simply be inserted into another function....
-
-
-
-
-
-
-
-
-
-
-
-//-------------Initiate-The-bangBox----------//
-//--this-begins-the-whole-shebang-mode---only-occurs-if-no-false-start//
-var initiateBang = function(){
-		window.setTimeout(bang, waitingPeriod);
-	};
-//-------------------------------------------//
-				
-				
-
-//-----Stops the BOX from appearing ------//
-var stopBang = function(){
-		clearTimeout(initiateBang);
-	};
-//-----------------------------------------//
-
-			
-
-//-----RESTART FUNCTION-----//
-var restart = function() {
-	waitingPeriod = Math.floor(Math.random() * (9000 - 3000)) + 3000;
-	startTime = 0;
-	readyButton.style.opacity = '1';
-	console.log("a fresh restart");
-
-};
 //-------------------------//
 
 
