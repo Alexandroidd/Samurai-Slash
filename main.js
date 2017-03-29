b52cc06beae001641bd863312334deae68e5e3b3
alexandroidd <alexandroidcreighton@gmail.com>
2017-03-28 22:04:59 -0600

    adds commenting and ordering of functions, some slight bug working happening.

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ var playerTwo = {
 ///////OBJECT DATA//////
 var readyButton = document.getElementById('ready');
 var isWorking = false;
-
+var badStart = false;
 
 
 
@@ -28,8 +28,30 @@ var isWorking = false;
 
 // --*This determines waiting time*--//
 var waitingPeriod = Math.floor(Math.random() * (9000 - 3000)) + 3000;
+//-------------------------------------
+
+
+
+// Here, "ready" button is active and initiates the 'timer'//
+//requires these vars: readyButton, falseStart, bang, waitingPeriod
+readyButton.addEventListener('click', function(){
+	console.log(waitingPeriod); //will be changed to run the timeout.
+	console.log("I'm Ready!");
+	document.addEventListener('click', falseStart);
+
+
+	window.setTimeout(bang, waitingPeriod);
+	readyButton.style.opacity = '0';
+
+});
+
+
+
 
 //---*tThis function makes the GO/ALERT box to shock player --//
+//----------REQUIRES the ready button to be clicked which ---//
+//------sets off the timeout for this to occure.-------------//
+
 var bang = function(){
 bangBox();
   console.log("BANG is Working!");
@@ -38,12 +60,16 @@ bangBox();
 
 //--Here the exclamation box is created--//
 bangBox = function() {
+	if (badStart === true) {
+		return;
+	} else {
 	var bangBox = document.createElement('img');
 	document.body.appendChild(bangBox);
 	isWorking = true;
 	bangBox.id="bang";
 	bangBox.src = "images/bang.png";
 	document.getElementById('bang').addEventListener("load", getTime);
+}
 	};
 
 
@@ -58,16 +84,16 @@ var getTime = function(event){
 };
 
 var earlyRestart = function(){
-	var removeBangs = document.getElementById('bang');
-	document.body.removeChild(removeBangs);
 	waitingPeriod = Math.floor(Math.random() * (9000 - 3000)) + 3000;
 	startTime = 0;
-	console.log("FALSE START TTRY AGAIN");
+	readyButton.style.opacity = '1';
+	console.log("FALSE START TRY AGAIN");
 };
 
 
 var falseStart = function() {
 if(isWorking !== true) {
+	badStart = true;
 	document.addEventListener('click', earlyRestart);     // -----add two variable that are each separate event listeners for the player's respective keys.
 } else {
 	return;
@@ -75,17 +101,7 @@ if(isWorking !== true) {
 };
 
 
-// Here, "ready" button is active and initiates the 'timer'//
-readyButton.addEventListener('click', function(){
-	console.log(waitingPeriod); //will be changed to run the timeout.
-	console.log("I'm Ready!");
-	document.addEventListener('click', falseStart);
 
-
-	window.setTimeout(bang, waitingPeriod);
-	readyButton.style.opacity = '0';
-
-});
 
 
 
@@ -104,10 +120,10 @@ var restart = function() {
 	document.body.removeChild(removeBangs);
 	waitingPeriod = Math.floor(Math.random() * (9000 - 3000)) + 3000;
 	startTime = 0;
-	
+	readyButton.style.opacity = '1';
 	console.log("a fresh restart");
 
-}
+};
 
 
 
