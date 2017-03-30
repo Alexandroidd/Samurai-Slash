342bc97dfae3a42226e2e24430f0ccd0c0c6a672
alexandroidd <alexandroidcreighton@gmail.com>
2017-03-29 20:16:58 -0600

    scoring system works

--- main.js
+++ main.js
@@ -6,34 +6,17 @@
 
 
 function falseStart() {
-	
-	console.log(clearTimeout(pause));
+	clearTimeout(pause);
 	console.log("A false start has been triggered");
 }
 
-
-var pause;
-
-var timeOutBang = function(){
-		pause = window.setTimeout(bangBox, waitingPeriod);
-	};
-	
-
-
-// document.addEventListener('keydown', function(event) {
-//	if(event.code === "KeyL") {
-
-var addListen = function(event) {
-	if(event.code === "KeyL" || event.code === "KeyA") {
-		falseStart(bangPause);
-	}
-};
-
+var pause;	
+var startTime;
 
 //-------------Initiate-The-bangBox----------//
 //--this-begins-the-whole-shebang-mode---only-occurs-if-no-false-start//
 var initiateBang = function(){
-		pause = window.setTimeout(bang, waitingPeriod);
+		pause = window.setTimeout(bangBox, waitingPeriod);
 			};
 //-------------------------------------------//
 				
@@ -43,7 +26,7 @@ var initiateBang = function(){
 //----This function gets the starting time of the Duel----//
 var getTime = function(event){
 	//this is where to save the timestamp into a variable
-	var startTime = Math.floor((event.timeStamp));
+	startTime = Math.floor((event.timeStamp));
 	console.log(startTime);
 	return(startTime);
 };
@@ -60,16 +43,6 @@ var restart = function() {
 	console.log("a restart has been initiated");
 
 };
-
-
-
-
-//IM THINKING THAT IF I CAN ATTACH THE EVENT LISTENER TO A SPECIFIC PART OF THE PLAYER OBJECT, 
-//I JUST DELETE THAT PART OF THE OBJECT LATER ON WHEN I NEED TO REMOVE THE EVENT LISTENER//
-
-
-
-
 
 // -----PLAYER DATA ----//
 var playerOne = {
@@ -95,13 +68,14 @@ var timeToAlert = false;
 var waitingPeriod = Math.floor(Math.random() * (9000 - 3000)) + 3000;
 //-------------------------------------
 
-// var addListen = function(x) {
-// 	document.addEventListener('keydown', function(event) {
-// 		if (event.code === "KeyA" || event.code === "KeyL") {
-// 			falseStart(x);
-// 		}
-// });
-// };
+
+
+function n() {
+	if (event.code === "KeyA" || event.code === "KeyL") {
+		falseStart();
+	}
+}
+
 
 // Here, "ready" button is active and initiates the 'timer'//
 //requires these vars: readyButton, falseStart, bang, waitingPeriod
@@ -110,57 +84,55 @@ readyButton.addEventListener('click', function(){
 	console.log("I'm Ready!");
       console.log("I'M LISTENING...");
       timeToAlert = true;
-      
      let interval = initiateBang();
-      // var kickOut = timeToGo();
-      document.addEventListener('keydown', function n(){
-      	if (event.code === "KeyA" || event.code === "KeyL") {
-      		falseStart();
-      	}
-      	});
+      document.addEventListener('keydown', n);
 
      // <--- can add function with two separate event listeners for each player's key.
 	readyButton.style.opacity = '0';
 });
 
 
+var playerOneTime;
+
+var playerTwoTime;
 
 
-//---------------------------//
-if(timeToAlert === true) {
-timeOutBang();
+
+function pOne() {
+	if (event.code === "KeyA") {
+		playerOneTime = Math.floor((event.timeStamp));
+		movesPlayed++;
+		evalWinner();
+		console.log(playerOneTime);
+	}
+}
+function pTwo() {
+	if (event.code === "KeyL") {
+		playerTwoTime = Math.floor((event.timeStamp));
+		movesPlayed++;
+		evalWinner();
+		console.log(playerTwoTime);
+	}
 }
 
-
-//This global var is weird, had//
-//to separate it though.
-//----------------------------//
-
-
-
-
-
-//---*tThis function makes the GO/ALERT box to shock player --//
-//----------REQUIRES the ready button to be clicked which ---//
-//------sets off the timeout for this to occur.-------------//
-var bang = function(){
-bangBox();
-  console.log("BANG is Working!");
-};
-//------------------------------------------------------------//
-
-//-----so, bang() just passes off the baton to bangBox, which actually----//
-//-----creates the box which exclaims that is is time to react------------//
-
+function evalWinner() {
+	if ( movesPlayed === 2 && (playerOneTime - startTime) < (playerTwoTime - startTime)) {
+		console.log("PLAYER ONE WINS");
+	} else if (movesPlayed === 2 && (playerTwoTime - startTime) < (playerOneTime - startTime)) {
+		console.log("Player Two WINS!");
+	}
+}
+var movesPlayed = 0;
 
 bangBox = function() {
-	// document.removeEventListener('keydown', );
+	document.removeEventListener('keydown', n);
 	var startBox = document.createElement('img');
 	document.body.appendChild(startBox);
 	startBox.id="bang";
 	startBox.src = "images/bang.png";
 	document.getElementById('bang').addEventListener("load", getTime);
-
+	document.addEventListener('keydown', pOne);
+	document.addEventListener('keydown', pTwo);
 
 	};
 
