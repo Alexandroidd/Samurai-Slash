0970554c171797f9553ba129ef6f4a163cbacb16
alexandroidd <alexandroidcreighton@gmail.com>
2017-06-06 14:38:52 -0600

    adds server file for heroku

---
+++ server.js
@@ -0,0 +1,13 @@
+var express      	= require('express');
+var app         	= express();
+var PORT			= process.env.PORT || 3000;
+
+
+
+
+app.use(express.static(__dirname + '/public'));
+
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port 3000 or somethign like that.");
+});
